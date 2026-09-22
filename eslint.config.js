c520a6ef3d5bf1c3d9f459c2a6bcf324ed9c7303
Yusuf-98 <yusuf.smg@gmail.com>
2026-09-22 19:42:05 +0700

    add: eslint and ci workflow

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    files: ['script.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'script',
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+        localStorage: 'readonly',
+        fetch: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
+  },
+];
